f7f315d84341da743e1223004465def440db0b25
Chris Collis <theotteryears@me.com>
2025-02-11 14:02:58 +0000

    Change .env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://wwnyngoadbgaggbfwqtl.supabase.co";
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3bnluZ29hZGJnYWdnYmZ3cXRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5OTQ2NTQsImV4cCI6MjA1MzU3MDY1NH0.xUNqPSz9YMLkK45zl1xLINzdlAc_U_anEpP54MQJK28";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
