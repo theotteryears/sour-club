0f93ec75e8e3597654a7787805d706de3c036f86
Chris Collis <theotteryears@me.com>
2025-02-11 13:54:07 +0000

    Change .env

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-	process.env.SUPABASE_DATABASE_URL,
-	process.env.SUPABASE_ANON_KEY,
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
